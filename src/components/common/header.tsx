dbec16c67e921e3baf0c492011aa09b0d3ae1fd1
Michael Hrizuk <hrizukmichael@gmail.com>
2020-08-24 19:31:20 -0400

    To create simple header component

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,2 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled(View)`
+  flex: 1;
+  backgroundColor: #fff;
+  alignItems: center;
+  justifyContent: center;
+`;
+
+export default function Header() {
+  return (
+    <Container>
+      <Text>I am a header</Text>
+    </Container>
+  )
+}
