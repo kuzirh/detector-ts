408ab7945a6465c362170092f3f6b9faa0967d1e
Michael Hrizuk <hrizukmichael@gmail.com>
2020-08-25 06:26:48 -0400

    added ability to navigate from home to camera screen and back

--- App.tsx
+++ App.tsx
@@ -4,14 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 // Custom Component Imports
 import Home from './src/components/screens/Home';
+import CameraScreen from './src/components/screens/Camera';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: any) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Camera"
+        onPress={() => navigation.navigate('Camera')}
+      />
     </View>
   );
 }
