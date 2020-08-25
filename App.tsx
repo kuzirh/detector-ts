f6e32294fb72de8855d8b42ad16dc8e0c14ea0e7
Michael Hrizuk <hrizukmichael@gmail.com>
2020-08-25 06:17:25 -0400

    moved HomeScreen out of main App and into it's own screen component

--- App.tsx
+++ App.tsx
@@ -1,36 +1,9 @@
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { Text, View } from 'react-native';
-// import styled from 'styled-components';
-
-// const Container = styled(View)`
-//   flex: 1;
-//   backgroundColor: #fff;
-//   alignItems: center;
-//   justifyContent: center;
-// `;
-
-// export default function App() {
-//   return (
-//     <Container>
-//       <Text>Open up App.tsx to start working on your app!</Text>
-//       <StatusBar style='auto' />
-//     </Container>
-//   );
-// }
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+// Custom Component Imports
+import Home from './src/components/screens/Home';
 
 const Stack = createStackNavigator();
 
@@ -38,7 +11,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
